ffc7d2a55d51d552ecd301754ccc5c4c45410431
Natalia <sh.nat.vlad@gmail.com>
2024-05-24 17:21:54 +0500

    refactor: create useHttp hook for sending requests

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,25 +1,22 @@
-import { useEffect, useState } from "react";
-
+import useHttp from "../hooks/useHttp.js";
 import MealItem from "./MealItem.jsx";
 
+const requestConfig = {};
+
 export default function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
-    
-            if (!response.ok) {
-                // ...
-            }
-    
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
+    const { 
+        data: loadedMeals,
+        isLoading,
+        error
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
+        
+    if (isLoading) {
+        return <p>Fetching meals...</p>
+    }
 
-        fetchMeals();
-    }, []);
-
-    
+    // if (!data) {
+    //     return <p>No meals found.</p>
+    // }
     
     return (
         <ul id="meals">
